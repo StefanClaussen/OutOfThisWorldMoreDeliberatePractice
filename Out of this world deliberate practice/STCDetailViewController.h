5cf5a03c70a04346ed616490c6c35d6a9421ae1f
StefenClausen <stefanclaussen@me.com>
2014-05-21 13:25:11 +0100

    Add detail view controller class

---
+++ Out of this world deliberate practice/STCDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCDetailViewController.h
+//  Out of this world deliberate practice
+//
+//  Created by Stefan Claussen on 21/05/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCDetailViewController : UIViewController
+
+@end
---
+++ Out of this world deliberate practice/STCDetailViewController.m
@@ -0,0 +1,49 @@
+//
+//  STCDetailViewController.m
+//  Out of this world deliberate practice
+//
+//  Created by Stefan Claussen on 21/05/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import "STCDetailViewController.h"
+
+@interface STCDetailViewController ()
+
+@end
+
+@implementation STCDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
