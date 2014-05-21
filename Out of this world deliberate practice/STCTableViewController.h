21a16bd773716310f46655541450c37352cdf9f6
StefenClausen <stefanclaussen@me.com>
2014-05-21 11:01:28 +0100

    Initial set up of table view

---
+++ Out of this world deliberate practice/STCTableViewController.h
@@ -0,0 +1,12 @@
+//
+//  STCTableViewController.h
+//  Out of this world deliberate practice
+
+#import <UIKit/UIKit.h>
+
+
+@interface STCTableViewController : UITableViewController
+
+@property (strong, nonatomic)NSMutableArray *planets;
+
+@end
